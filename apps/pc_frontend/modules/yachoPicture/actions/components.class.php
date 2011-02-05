2fef2db076505757402cb79729605ff0daab8335
shingo <shingo@l234133.dynamic.ppp.asahi-net.or.jp>
2011-02-05 15:18:06 +0900

    リファクタリング & ファンクションキャッシュ対応

--- apps/pc_frontend/modules/yachoPicture/actions/components.class.php
+++ apps/pc_frontend/modules/yachoPicture/actions/components.class.php
@@ -10,212 +10,67 @@ class yachoPictureComponents extends sfComponents
 {
   public function executeYachoPictureHomeList()
   {
-    //検索文字列の設定
-    $toriList = array(
-      'メジロ',
-      'カワセミ',
-      'オオルリ',
-      'アカゲラ',
-      'ハクセキレイ',
-      'ヒバリ',
-      'オシドリ',
-      'ウグイス',
-      'シジュウカラ',
-      'モズ',
-      'ウズラ',
-      'タンチョウヅル',
-      'コハクチョウ',
-      'キジ',
-      'ヤマドリ',
-      'キビタキ',
-      'シラコバト',
-      'ホオジロ',
-      'ユリカモメ',
-      'カモメ',
-      'ライチョウ',
-      'ツグミ',
-      'サンコウチョウ',
-      'コノハズク',
-      'シロチドリ',
-      'カイツブリ',
-      'オオミズナギドリ',
-      'コウノトリ',
-      'オオハクチョウ',
-      'ホトトギス',
-      'ナベヅル',
-      'コマドリ',
-      'カササギ',
-      'ミサゴ',
-      'トビ',
-      'オジロワシ',
-      'オオワシ',
-      'オオタカ',
-      'ハイタカ',
-      'サシバ',
-      'ノスリ',
-      'ハイイロチュウヒ',
-      'ハヤブサ',
-      'チョウゲンボウ',
-      'コジュケイ',
-      'キジバト',
-      'アオバト',
-      'ジュウイチ',
-      'ツツドリ',
-      'カッコウ',
-      'シマフクロウ',
-      'トラフズク',
-      'コミミズク',
-      'アオバズク',
-      'フクロウ',
-      'ヨタカ',
-      'アマツバメ',
-      'ヤマショウビン',
-      'アカショウビン',
-      'ヤマセミ',
-      'ブッポウソウ',
-      'ヤツガシラ',
-      'アオゲラ',
-      'ヤマゲラ',
-      'クマゲラ',
-      'オオアカゲラ',
-      'コゲラ',
-      'コシアカツバメ',
-      'ツバメ',
-      'イワツバメ',
-      'ツメナガセキレイ',
-      'キセキレイ',
-      'セグロセキレイ',
-      'ビンズイ',
-      'タヒバリ',
-      'ヒヨドリ',
-      'サンショウクイ',
-      'チゴモズ',
-      'アカモズ',
-      'キレンジャク',
-      'ヒレンジャク',
-      'カワガラス',
-      'ミソサザイ',
-      'イワヒバリ',
-      'カヤクグリ',
-      'アカヒゲ',
-      'ノゴマ',
-      'コルリ',
-      'ルリビタキ',
-      'ノビタキ',
-      'ジョウビタキ',
-      'イソヒヨドリ',
-      'トラツグミ',
-      'マミジロ',
-      'クロツグミ',
-      'アカバラ',
-      'アカコッコ',
-      'シロハラ',
-      'マミチャジナイ',
-      'ヤブサメ',
-      'オオセッカ',
-      'シマセンニュウ',
-      'マキノセンニュウ',
-      'コヨシキリ',
-      'オオヨシキリ',
-      'メボソムシクイ',
-      'エゾムシクイ',
-      'センダイムシクイ',
-      'キクイタダキ',
-      'セッカ',
-      'ムギマキ',
-      'サメビタキ',
-      'コサメビタキ',
-      'エゾビタキ',
-      'エナガ',
-      'コガラ',
-      'ヒガラ',
-      'ヤマガラ',
-      'ゴジュウカラ',
-      'コチドリ',
-      'キバシリ',
-      'メグロ',
-      'コジュリン',
-      'シロハラホオジロ',
-      'ホオアカ',
-      'コホオアカ',
-      'キマユホオジロ',
-      'カシラダカ',
-      'ミヤマホオジロ',
-      'シマアオジ',
-      'シマノジコ',
-      'ノジコ',
-      'アオジ',
-      'クロジ',
-      'オオジュリン',
-      'アトリ',
-      'カワラヒワ',
-      'マヒワ',
-      'ベニヒワ',
-      'ハギマシコ',
-      'オオマシコ',
-      'ギンザンマシコ',
-      'イスカ',
-      'ベニマシコ',
-      'ウソ',
-      'シメ',
-      'イカル',
-      'ニュウナイスズメ',
-      'スズメ',
-      'コムクドリ',
-    );
-    $key = array_rand($toriList);
-    $toriName = $toriList[$key];
-
-    $linkUrl = 'http://ja.wikipedia.org/wiki/';
-    $imageWidth = 250;
-
-
-    $urlString = $toriName;
-    //urlエンコード
-    $urlString = urlencode($urlString);
-    $linkUrl .= $urlString;
-
-    $url = "http://ajax.googleapis.com/ajax/services/search/images";
-    $url = $url."?v=1.0";
-    $url = $url."&key=ABQIAAAAOPtPI5yGL-nFD6Z-kVoJxxQmO4LSR-cwvAgRQxlrUu2LJmLSXBRZ6rnUtzFGACVM0FRb2UWlx4CCpg";
-    $url = $url."&rsz=large";
-    $url = $url."&q=".$urlString;
-
-    // sendRequest
-    // note how referer is set manually
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_REFERER, "http://factory.uzuralife.com/labo/google_search_api_img_onserv.php");
-    $body = curl_exec($ch);
-    curl_close($ch);
-
-    // now, process the JSON string
-    $json = json_decode($body);
-    //ステータスコードが200以外の場合はエラー
-    if($json->responseStatus != 200)
+    $pictures = $this->getYachoPictures('contents');
+    foreach ($pictures as $key => $value)
     {
-      //エラー処理を記述
-      //exit();
+      $this->$key = $value;
     }
-
-    $responseData = $json->responseData;
-    $results = $responseData->results;
-    if(!$results)
-    {
-      //エラー処理を記述
-      //exit();
-    }
-
-    $this->toriName = $toriName;
-    $this->linkUrl = $linkUrl;
-    $this->imageWidth = $imageWidth;
-
-    $i = mt_rand(0, 7);
-    $this->imageUrl = $results[$i]->url;
   }
 
   public function executeYachoPictureSideList()
+  {
+    $pictures = $this->getYachoPictures();
+    foreach ($pictures as $key => $value)
+    {
+      $this->$key = $value;
+    }
+  }
+
+  public function getYachoPictures($type = 'side')
+  {
+    $res = array();
+
+    $toriName = $this->getRandamYachoName();
+    $res['toriName']   = $toriName;
+
+    $imageWidth = 150;
+    if (sfConfig::get('app_tori_yacho_picture_image_width_'.$type))
+    {
+      $imageWidth = sfConfig::get('app_tori_yacho_picture_image_width_'.$type);
+    }
+    $res['imageWidth'] = $imageWidth;
+
+    $linkUrl = '';
+    if ($isLink = sfConfig::get('app_tori_yacho_picture_is_enable_wiki_link'))
+    {
+      $linkUrl  = 'http://ja.wikipedia.org/wiki/';
+      $linkUrl .= urlencode($this->toriName);
+    }
+    $res['linkUrl'] = $linkUrl;
+
+    $imageUrl = '';
+    if (sfConfig::get('app_tori_yacho_picture_is_enable_search_result_cache'))
+    {
+      $cache = new sfFileCache(array('cache_dir' => sfConfig::get('sf_cache_dir').'/function'));
+      $fc = new sfFunctionCache($cache);
+      $results = $fc->call(array('opToriYachoPicturePluginToolkit', 'getYachoPictureSearchResults'), array($toriName));
+    }
+    else
+    {
+      $results = opToriYachoPicturePluginToolkit::getYachoPictureSearchResults($toriName);
+    }
+
+    if ($results)
+    {
+      $i = mt_rand(0, count($results));
+      $imageUrl = $results[$i]->url;
+    }
+    $res['imageUrl'] = $imageUrl;
+
+    return $res;
+  }
+
+  protected function getRandamYachoName()
   {
     //検索文字列の設定
     $toriList = array(
@@ -371,55 +226,7 @@ class yachoPictureComponents extends sfComponents
       'コムクドリ',
     );
     $key = array_rand($toriList);
-    $toriName = $toriList[$key];
 
-    $linkUrl = 'http://ja.wikipedia.org/wiki/';
-    $imageWidth = 150;
-
-
-    $urlString = $toriName;
-    //urlエンコード
-    $urlString = urlencode($urlString);
-    $linkUrl .= $urlString;
-
-    $url = "http://ajax.googleapis.com/ajax/services/search/images";
-    $url = $url."?v=1.0";
-    $url = $url."&key=ABQIAAAAOPtPI5yGL-nFD6Z-kVoJxxQmO4LSR-cwvAgRQxlrUu2LJmLSXBRZ6rnUtzFGACVM0FRb2UWlx4CCpg";
-    $url = $url."&rsz=large";
-    $url = $url."&safe=active";
-    $url = $url."&q=".$urlString;
-
-    // sendRequest
-    // note how referer is set manually
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_REFERER, "http://factory.uzuralife.com/labo/google_search_api_img_onserv.php");
-    $body = curl_exec($ch);
-    curl_close($ch);
-
-    // now, process the JSON string
-    $json = json_decode($body);
-    //ステータスコードが200以外の場合はエラー
-    if($json->responseStatus != 200)
-    {
-      //エラー処理を記述
-      //exit();
-    }
-
-    $responseData = $json->responseData;
-    $results = $responseData->results;
-    if(!$results)
-    {
-      //エラー処理を記述
-      //exit();
-    }
-
-    $this->toriName = $toriName;
-    $this->linkUrl = $linkUrl;
-    $this->imageWidth = $imageWidth;
-
-    $i = mt_rand(0, 7);
-    $this->imageUrl = $results[$i]->url;
+    return $toriList[$key];
   }
 }
