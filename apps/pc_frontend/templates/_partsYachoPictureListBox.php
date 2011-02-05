890fd8e30c6ad6822e69df0ce0ba29e83cc0ad82
shingo <shingo@l234133.dynamic.ppp.asahi-net.or.jp>
2011-02-05 15:30:48 +0900

    リンクにするかどうかの設定値に対応

--- apps/pc_frontend/templates/_partsYachoPictureListBox.php
+++ apps/pc_frontend/templates/_partsYachoPictureListBox.php
@@ -10,8 +10,13 @@
 <table>
 <tbody>
 
+<?php if (sfConfig::get('app_tori_yacho_picture_is_enable_wiki_link')): ?>
 <p style="text-align:center;"><a href="<?php echo $options['linkUrl']; ?>" target="_blank"><img src="<?php echo $options['imageUrl']; ?>" width="<?php echo $options['imageWidth']; ?>" style="display:none" onload="this.style.display='inline'"></a></p>
 <p style="text-align:center;margin-top:10px;"><strong><a href="<?php echo $options['linkUrl']; ?>" target="_blank"><?php echo $options['toriName']; ?></a></strong></p>
+<?php else: ?>
+<p style="text-align:center;"><img src="<?php echo $options['imageUrl']; ?>" width="<?php echo $options['imageWidth']; ?>" style="display:none" onload="this.style.display='inline'"></p>
+<p style="text-align:center;margin-top:10px;"><strong><?php echo $options['toriName']; ?></strong></p>
+<?php endif; ?>
 
 <?php if (isset($options['moreInfo'])): ?>
 <div class="moreInfo"><ul class="moreInfo"><li>
